3369e7d76061c28ef79ee54b3b496d7014f01892
kierancoito <keirancoito@gmail.com>
2019-10-06 15:31:05 -0700

    Added ability to interact with html and extract first link

--- WebCrawler/Program.cs
+++ WebCrawler/Program.cs
@@ -1,22 +1,74 @@
 ﻿using System;
 using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Net.Http;
 
 namespace WebCrawler
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Making API Call...");
+            string[] arguments; 
+            arguments = new string[2]{"https://stackoverflow.com/questions/15641797/extract-base-url-from-a-string-in-c", "4" };
             
-            using (var client = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate }))
-            {
-                client.BaseAddress = new Uri("http://courses.washington.edu/");
-                HttpResponseMessage response = client.GetAsync("css342/dimpsey").Result;
-                response.EnsureSuccessStatusCode();
-                string result = response.Content.ReadAsStringAsync().Result;
-                Console.WriteLine("Result: " + result);
+            //verify the correct number of arguments
+            if (arguments.Length != 2) {
+                Console.WriteLine("Not enough valid arguments to run application");
+                return;
+            }
+
+            int hops = int.Parse(arguments[1]);
+            //verify valid number of hops
+            if (hops <= 0) {
+                Console.WriteLine("invalid number of hops specified to run application");
+                return;
+            }
+            
+            //extract initial base URI and the URI path 
+            var uri = new Uri(arguments[0]);
+            var baseUri = uri.GetLeftPart(System.UriPartial.Authority);
+            var extension = uri.PathAndQuery;
+            
+            Console.WriteLine(baseUri);
+            Console.WriteLine(extension);
+            Console.WriteLine(baseUri + extension);
+            Console.WriteLine("making call");
+
+            while (hops != 0) {
+                
+                using (var client = new HttpClient(new HttpClientHandler
+                    {AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate})) {
+                    
+                    //set base address for HTTP client
+                    client.BaseAddress = new Uri(baseUri);
+                    //get response from website at the specified extension
+                    HttpResponseMessage response = client.GetAsync(extension).Result;
+                    
+                    //check if page exists
+                    if (response.IsSuccessStatusCode) {
+                        //handle success
+                        string result = response.Content.ReadAsStringAsync().Result;
+
+                        //setup regex 
+                        var linkParser = new Regex(@"http(s)?://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)?", 
+                            RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                        
+                        //find first URL
+                        Console.WriteLine(linkParser.Match(result));
+                        
+                    } else {
+                        //handle failure
+                        int failureCode = (int) response.StatusCode;
+                        Console.WriteLine(failureCode);
+                        return;
+                    }
+
+                    Console.WriteLine(hops);
+                }
+
+                hops--;
             }
         }
     }
