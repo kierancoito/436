c084e2e977ab049c0158815b2e03b9eeec011b3c
kierancoito <keirancoito@gmail.com>
2019-11-01 21:28:13 -0700

    fixed some small issuues

--- BackUp/Backup.py
+++ BackUp/Backup.py
@@ -5,16 +5,19 @@ import datetime
 from datetime import datetime
 from botocore.exceptions import ClientError
 
-import http
+"""
+Created by Kieran Coito
+CSS 436
+November 1st 2019
+
+Assignment 3
+
+This program will back up to AWS the active directory that the program is executed in. It will not upload itself or 
+the metaData.txt it creates to facilitate the uploading.
 
 """
-Initial File creation
-
-
-"""
-
+#name of file to save relevant info for uploading
 metaDataText = "metaData.txt"
-bucketName = "bucket" + str(datetime.now().strftime('%Y%m%d%H%M%S'))
 
 def backer():
     newSave = False
@@ -22,42 +25,49 @@ def backer():
     #set up client to upload files, and create new bucket to upload files to
     client = boto3.client('s3')
 
-    try:
-        client.create_bucket(Bucket= bucketName, CreateBucketConfiguration={'LocationConstraint': 'us-west-2' },)
-    except ClientError as e:
-        if e.response['Error']['Code'] == 'BucketAlreadyExists':
-            print("That bucket already exists and cannot be created and uploaded to")
-            return 0
-        if e.response['ResponseMetadata']['HTTPStatusCode'] == 500:
-            print("Could not connect to service will retry")
-            return -1
-        else:
-            print("An error occurred while trying to connect to the client, please check credentials and rerun")
-            return 0
-
-    print(" ")
-    print("The bucket named " + bucketName + " was created to store your backup")
-    print(" ")
-
     #check if there is already a meta data file or not, which dictates if this is the first run or not
     if not os.path.exists(metaDataText):
-        metaData = open(metaDataText, "w+")
-        metaData.write(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+        metaData = open(metaDataText, "w")
+        #get current time of application running and save it for future reference
+        lastupdate = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        metaData.write( str(lastupdate) + "\n")
+        #create new bucket name and save it for future reference
+        bucketName = "backupBucket_" + str(datetime.now().strftime('%Y%m%d%H%M%S'))
+        metaData.write(bucketName)
         newSave = True
     else:
-        metaData = open(metaDataText, "w+")
+        metaData = open(metaDataText, "r+")
         #get time of last upload
-        lastUpdatedTime = metaData.readline()
+        lastUpdatedTime = str(metaData.readline())
         if lastUpdatedTime != '':
-            datetime.strptime(lastUpdatedTime, '%Y-%m-%d %H:%M:%S')
+            lastupdate = datetime.strptime(lastUpdatedTime[0:len(lastUpdatedTime)-1], '%Y-%m-%d %H:%M:%S')
+        #get bucket name that is being backed up to
+        bucketName = str(metaData.readline())
         metaData.seek(0)
         metaData.truncate()
         #update file to current time as new upload is happening
-        metaData.write(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+        metaData.write(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "\n")
+        metaData.write(bucketName)
 
     s3 = boto3.resource('s3')
+    if newSave:
+        try:
+            client.create_bucket(Bucket=bucketName, CreateBucketConfiguration={'LocationConstraint': 'us-west-2'}, )
+        #handle specific exceptions that may happen
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'BucketAlreadyExists':
+                print("That bucket already exists and cannot be created and uploaded to")
+                return 0
+            if e.response['ResponseMetadata']['HTTPStatusCode'] == 500:
+                print("Could not connect to service will retry")
+                return -1
+            else:
+                print("An error occurred while trying to connect to the client, please check credentials and rerun")
+                return 0
 
-    print("uploading starting")
+    print(" ")
+    print("The bucket named " + bucketName + " will store your backup")
+    print("upload starting")
     print(" ")
     #iterate through all files in the current directory and down
     for dirname, dirnames, filenames in os.walk('.'):
@@ -66,16 +76,13 @@ def backer():
             fullpath = str(os.path.join(dirname, filename))
             length = len(fullpath)
 
-            #skip over metadata used for this file and this file itself
-            if filename == "Backup.py" or filename == "metaData.txt":
-                continue
-
             #the current file that will be uploaded, get last time it was updated
             currentFileUpdateTime = os.path.getmtime(fullpath)
             updateTime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(currentFileUpdateTime))
+            fileTime = datetime.strptime(updateTime, '%Y-%m-%d %H:%M:%S')
 
             #only upload of this is a new save or the file has been edited since the last upload
-            if newSave or updateTime > lastUpdatedTime:
+            if newSave or (fileTime > lastupdate):
                 #attempt to upload file, catch any errors and handle appropriately
                 try:
                     s3.Object(bucketName, 'Backups' + fullpath[1:length] )\
@@ -89,13 +96,10 @@ def backer():
                             "please check credentials and file permissions and rerun")
 
                 print(filename + " uploaded to " 'Backups' + fullpath[1:length] )
-    print(" ")
     print("Uploading has finished")
     return 0
 
-print(" ")
-print(" ")
-print(" ")
+print("\n")
 #run back up program
 retry = backer()
 #if backer returns a -1 than there was a connection issue and it should run backer one more time
